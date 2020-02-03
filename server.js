eaf29dd81a84c5f576d183ddaa25ab6c1eeac930
David Francis <davidjayfrancis@gmail.com>
2020-02-03 01:20:21 -0800

    built out routing and endpoints

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+// const db = require("./data/dbConfig.js");
+
+const accountsRouter = require("./accounts/accounts-router.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/accounts", accountsRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h3>Life is good</h3>");
+});
 
 module.exports = server;
